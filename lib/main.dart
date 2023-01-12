c7b0b8facd1da90a40400e75bfa8dbcbebf674c2
kipkoechke <chepkwonyke1@gmail.com>
2023-01-12 22:55:27 +0300

    Defined the overall look and feel of the app

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,21 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        canvasColor: const Color.fromRGBO(255, 245, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold,
+          ),
+          bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+        ),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+            .copyWith(secondary: Colors.amber),
       ),
+
       home: const CategoriesScreen(),
     );
   }
