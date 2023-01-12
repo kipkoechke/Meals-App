879d8d98ccbce04de121de1d9792cf4a71c33780
kipkoechke <chepkwonyke1@gmail.com>
2023-01-12 21:27:34 +0300

    Added base data

---
+++ lib/categories_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: [],
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200.0,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20.0,
+        mainAxisSpacing: 20.0,
+      ),
+    );
+  }
+}
