520ec1fd46ac9252fae76a96c5ff4f9fea6182fa
kipkoechke <chepkwonyke1@gmail.com>
2023-01-15 17:54:12 +0300

    Created a screen which shows meal detail

---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/meal-screen';
+
+  const MealDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final mealId = ModalRoute.of(context)!.settings.arguments as String;
+    return Scaffold(
+      body: Center(
+        child: Text('The meal - $mealId'),
+      ),
+    );
+  }
+}
