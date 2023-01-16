78990509ae2a68b0fe3cd17b8bcb47c7b9c0bc77
kipkoechke <chepkwonyke1@gmail.com>
2023-01-16 16:51:36 +0300

    Modified the container

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -45,7 +45,7 @@ class MealDetailScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
+            SizedBox(
               height: 300,
               width: double.infinity,
               child: Image.network(
